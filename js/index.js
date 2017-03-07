75db486d37207ffd057a0a2b820bbd8c08d199aa
wxt007726 <491915069@qq.com>
2017-03-07 22:36:37 +0800

    333

---
+++ js/index.js
@@ -0,0 +1,56 @@
+(function(){
+  //页面
+  var page1 = function(){
+    return `
+    <div class="page1">
+      <table>
+        <tr>
+          <td>姓名：</td>
+          <td>王小天</td>
+        </tr>
+      </table>
+    </div>
+    `
+  };
+  var page2 = function(){
+    return
+  };
+  var page3 = function(){
+    return
+  };
+
+  var page4 = function(){
+    return
+  };
+  var arr = [page1,page2,page3,page4]
+
+  //页面切换
+  $('#content').html(arr[0]);
+
+  $('#nav').on('click','li',function(){
+    $('#nav li').removeClass('selected name');
+    $(this).toggleClass('selected');
+    index=$(this).index();
+    $('#content').html('');
+    $('#content').html(arr[index]);
+    $.ajax({
+      url: 'data/data.json',
+      type: 'GET',
+      dataType: 'json',
+      complete: function() {
+        console.log('compelete');
+      },
+      success:function(data){
+        console.log(data);
+      }
+    })
+    .done(function() {
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    });
+
+  });
+})();
+//加载页面
